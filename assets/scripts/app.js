0c3631b1e485cdd62d3e263d6bff72e52dd7092e
HajimeNoJosh <joshuah.m.martin@gmail.com>
2019-09-25 11:28:48 -0400

    Add Game Logic
    At the moment the idea is that the divs are cells that I attach
    a player value to. The current issue is making it so that only one
    cell changes, and the specific cell I am clicking on.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,7 +5,31 @@
 
 // use require without a reference to ensure a file is bundled
 // require('./example')
+let playerTurn = true
+
+function checkTurn () {
+  let player = ''
+  if (playerTurn) {
+    player = 'x'
+  } else {
+    player = 'o'
+  }
+  playerTurn = !playerTurn
+  return player
+}
+
+const changeText = function () {
+  const player = checkTurn()
+  $('div').first().text(player)
+}
+
+const reset = function () {
+  playerTurn = true
+  $('div').text('')
+}
+
 
 $(() => {
-  // your JS code goes here
+  $('div').on('click', changeText)
+  $('.click').on('click', reset)
 })
